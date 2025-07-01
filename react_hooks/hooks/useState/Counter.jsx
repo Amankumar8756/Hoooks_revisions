35a7b2f948a029f9d949a22a79ba90ce2e55e2e4
Aman kumar <iit2022168@iiita.ac.in>
2025-07-01 20:50:55 +0530

    counter

--- react_hooks/hooks/useState/Counter.jsx
+++ react_hooks/hooks/useState/Counter.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
 
 const Counter = () => {
+
+    let counter =0;
+    
+    const increase = () =>{
+        counter++;
+        console.log("counter increase = ",counter)
+    }
+
+    const decrease =()=>{
+        counter--;
+          console.log("counter decrease = ",counter)
+    }
+
   return (
-    <div>Counter</div>
+    <div>
+        <h1>Counters</h1>
+       <div className='count'>
+         <h2>{counter}</h2>
+
+         <button onClick={increase}>increase by 1</button>
+
+         <button onClick={decrease}>increase by 2</button>
+       </div>
+
+    </div>
   )
 }
 
